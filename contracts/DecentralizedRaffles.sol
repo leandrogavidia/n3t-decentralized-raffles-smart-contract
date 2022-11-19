c925d346d74f63785df29e60657c8a481f651f0c
leandrogavidia <leandergs1234@gmail.com>
2022-11-18 21:50:10 -0400

    Add function and validation to add participants, add function to get total participants

--- contracts/DecentralizedRaffles.sol
+++ contracts/DecentralizedRaffles.sol
@@ -4,9 +4,26 @@ pragma solidity >= 0.7.0 < 0.9.0;
 import "hardhat/console.sol";
 
 contract DecentralizedRaffles {
-    uint[50] participants;
+    uint256 raffleDate;
+    address[] participants;
+    uint256 moneyCollected = 0;
+    string awards;
+    address ownerAddress;
 
     constructor() {
-        console.log("My address", msg.sender);
+        ownerAddress = msg.sender;
+    }
+
+    modifier maxParticipants() {
+        require(participants.length < 20, ":( Sorry, there is no room for more participants. Please wait for the next raffle.");
+        _;
+    }
+
+    function addParticipate(address _participant) public payable maxParticipants {
+        participants.push(_participant);
+    }
+
+    function getTotalParticipants() public view returns(uint256) {
+        return participants.length;
     }
 }
